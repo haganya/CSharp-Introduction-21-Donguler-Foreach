6edc779c4f61f4d1af8d5b11424edc4f7625235b
Hakan Akgül <hakanakgul34@gmail.com>
2017-09-26 20:22:36 +0300

    created at 09.08.2017 (dd.mm.yyyy)

---
+++ 20170809-4Con_ForeachDongusu/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _20170809_4Con_ForeachDongusu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] isimler = new string[20];
+            for (int i = 0; i < isimler.Length; i++)
+            {
+                Console.Write((i+1)+". ismi giriniz : ");
+                isimler[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("-----------------------------------------------");
+
+            //for (int i = 0; i < isimler.Length; i++)
+            //{
+            //    Console.WriteLine(isimler[i]);
+            //}
+            
+
+            /*
+             * foreach döngüsü
+             * 1. elemandan son elemana doğru sıralı olarak gider.
+             * sadece ileri doğru okuyabilir. tersine okuyamaz
+             * readonly özelliği vardır. döngü değişkeni sadece okunuz. içine değer atanamaz.
+             * for döngüsüne göre daha yeteneksizdir. 
+             */
+
+            foreach (string item in isimler)
+            {
+                //eleman = "a";  //foreach değişkene değer atılamaz.
+                Console.WriteLine(item);
+            }
+
+            Console.Read();
+
+        }
+    }
+}
